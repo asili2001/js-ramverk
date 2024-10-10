f9a6f6deb3bc38021a902ceb797643925e1fc319
Axjo21 <117208303+Axjo21@users.noreply.github.com>
2024-10-10 22:17:24 +0200

    Add sockets to backend, not functional yet

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,5 +32,41 @@ connectDB();
 app.use('/api/users', userRoutes);
 app.use('/api/documents', documentRoutes);
 
+
+
+// Socket imports
+const httpServer = require("http").createServer(app);
+const socketioJwt = require('socketio-jwt');
+const createRoom = require("./socket/socket.io.js");
+const handleDoc = require("./socket/socket.io.js");
+
+
+// Socket setup
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: ['http://localhost:5173', 'https://inker.ahmadasi.li', 'http://127.0.0.1:5173'],
+        methods: ["GET", "POST"]
+    }
+});
+
+
+// Socket events
+io.sockets.on('connection', socketioJwt.authorize({
+        secret: 'SECRET_KEY',
+        timeout: 15000
+    })).on('authenticated', function(socket) {
+        console.log(`Socket Authenticated! ${socket.decoded_token.name}`);
+        console.log(socket.id);
+        
+        socket.on('create', createRoom(io, socket, room));
+        socket.on("doc", handleDoc(io, socket, data));
+
+        socket.on('disconnect', () => {
+            console.log(`Client disconnected: ${socket.id}`);
+        });
+});
+
+
+
 // Start up server
 app.listen(process.env.PORT, () => console.log(`INKER API listening on port ${process.env.PORT}!`));
