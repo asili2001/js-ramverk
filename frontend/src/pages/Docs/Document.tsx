793e1038d4c3c7f4f90daf588a859b7c3920d073
Ahmad Asili <ahmadasili1928@gmail.com>
2024-10-13 15:17:41 +0200

    Frontend Implementation of socket and Document flow. Implement Auth Check middleware for socket

--- frontend/src/pages/Docs/Document.tsx
+++ frontend/src/pages/Docs/Document.tsx
@@ -5,11 +5,13 @@ import './main.scss';
 import { useEffect, useState } from 'react';
 import useAPIDocs from '../../hooks/useAPIDocs';
 import LoadingSpinner from '../../components/Loading';
+import useDocSocket, { RTextUpdateGeneral, TextDelete, TextInsert, TextReplace, TextUpdateGeneral } from '../../hooks/useDocSocket';
 
 const Document = () => {
 	const { documentId } = useParams();
 	const [title, setTitle] = useState('');
 	const [content, setContent] = useState('');
+	const [recivedUpdate, setRecivedUpdate] = useState<RTextUpdateGeneral|null>(null);
 	const [loadedDoc, setLoadedDoc] = useState(false);
 	const { getDoc, updateDoc, isLoading } = useAPIDocs();
 	const navigate = useNavigate();
@@ -30,9 +32,42 @@ const Document = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [documentId]);
 
-	const handleDocumentChange = async (draftContent: string) => {
-		if (documentId) {
-			await updateDoc(documentId, undefined, draftContent);
+	const handleSocketUpdate = (recived: RTextUpdateGeneral) => {
+		if (recived.owner !== socket.current.id) {
+			setRecivedUpdate(recived);
+		}
+	};
+
+	// Use the WebSocket hook for handling document updates
+	if (!documentId) return;
+	const { socket, insertText, deleteText, replaceText } = useDocSocket(documentId, handleSocketUpdate);
+	
+
+	const handleDocumentChange = async (data: TextUpdateGeneral) => {
+		if (data.changeType === "replace") {
+			const replaceChange = data as TextReplace;
+			if (!replaceChange.changedText) return;
+			replaceText({
+				content: replaceChange.changedText,
+				startIndex: replaceChange.startIndex,
+				endIndex: replaceChange.endIndex
+			})
+		}
+		else if (data.changeType === "insert") {
+			const insertChange = data as TextInsert;
+			if (!insertChange.changedText) return;
+			insertText({
+				content: insertChange.changedText,
+				startIndex: insertChange.startIndex,
+				endIndex: insertChange.endIndex
+			})
+		}
+		else if (data.changeType === "delete") {
+			const deleteChange = data as TextDelete;
+			deleteText({
+				startIndex: deleteChange.startIndex,
+				endIndex: deleteChange.endIndex
+			})
 		}
 	};
 	const handleTitleChange = async (newTitle: string) => {
@@ -43,7 +78,7 @@ const Document = () => {
 	};
 
 	return (
-		loadedDoc && (
+		loadedDoc && documentId && (
 			<div className="document-page">
 				<DocumentNavbar
 					documentTitle={title}
@@ -53,6 +88,7 @@ const Document = () => {
 					content={content}
 					className="main-textbox"
 					onChange={handleDocumentChange}
+					recivedUpdate={recivedUpdate}
 					editable
 				/>
 				{isLoading && <LoadingSpinner floating />}
--- frontend/src/pages/Docs/index.tsx
+++ frontend/src/pages/Docs/index.tsx
@@ -19,8 +19,6 @@ const Documents = () => {
 	const handleNewDocCreation = async () => {
 		const newDocument = await newDoc();
 		if (newDocument) {
-			console.log(newDocument);
-
 			navigate(`/documents/${newDocument.id}`);
 		}
 	};
